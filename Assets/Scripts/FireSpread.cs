841b433ef9eb1dd4f0673541159be007dc7eff84
Chris Domingos <chrisd03@outlook.com>
2023-02-17 16:59:52 -0500

    setting a foundation
    Starting firespread

--- Assets/Scripts/FireSpread.cs
+++ Assets/Scripts/FireSpread.cs
@@ -2,27 +2,38 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Fire : MonoBehaviour
+public class FireSpread : MonoBehaviour
 {
 
     private SpriteRenderer sr;
 
     public float aReduction = 0.001f;
     public float lifeTime = 3f;
+    GameObject tree;
 
     private float lifeStart;
+
+    bool justLit;
+    bool onFire;
+    bool putOut;
 
 
     // Start is called before the first frame update
     void Start()
     {
+
         sr = GetComponent<SpriteRenderer>();
         lifeStart = Time.time;
+        tree = GameObject.FindGame
     }
 
     // Update is called once per frame
     void Update()
     {
+        void
+        if(collision.gameObject.name == "Arsonist"){
+            onFire = true;
+        }
         if (Time.time >= lifeStart + lifeTime)
         {
             Destroy(gameObject);
