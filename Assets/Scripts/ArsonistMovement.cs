1ede1d64982e9ea63b7afe188eb54b3921940074
JoeyGiordano <jgzgiordano@gmail.com>
2023-03-27 23:54:29 -0400

    Messing with the arsonist

--- Assets/Scripts/ArsonistMovement.cs
+++ Assets/Scripts/ArsonistMovement.cs
@@ -5,25 +5,36 @@ using UnityEngine;
 public class ArsonistMovement : MonoBehaviour
 {
     public float rotateSpeed = 1, moveSpeed = 1;
+    public float baseRotateSpeed = 160, highRotateSpeed = 250, extraSpeed, decay;
+
+    //Rigidbody2D rb;
 
     // Start is called before the first frame update
     void Start()
     {
         GetComponentInChildren<Fuel>().enabled = false;
+        //rb.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.D))
+        {
+            extraSpeed = highRotateSpeed;
+        }
+
+
         if (Input.GetKey(KeyCode.A))
         {
-            transform.Rotate(rotateSpeed * new Vector3(0, 0, 1) * Time.deltaTime);
+            extraSpeed *= decay;
+            transform.Rotate((baseRotateSpeed+extraSpeed) * new Vector3(0, 0, 1) * Time.deltaTime);
         }
 
         if (Input.GetKey(KeyCode.D))
         {
-            transform.Rotate(-1 * rotateSpeed * new Vector3(0, 0, 1) * Time.deltaTime);
+            extraSpeed *= decay;
+            transform.Rotate(-1 * (baseRotateSpeed + extraSpeed) * new Vector3(0, 0, 1) * Time.deltaTime);
         }
 
         transform.position += moveSpeed * transform.up * Time.deltaTime;
@@ -36,5 +47,9 @@ public class ArsonistMovement : MonoBehaviour
         {
             GetComponentInChildren<Fuel>().enabled = true;
         }
+
+
+
+
     }
 }
