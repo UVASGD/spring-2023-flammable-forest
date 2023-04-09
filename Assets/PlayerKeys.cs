0cc18e9ee7a6a6b2c547581e83e17fa8dbbf9f71
ericli5 <lieric021@gmail.com>
2023-04-09 15:16:30 -0400

    Tile Changing

--- Assets/PlayerKeys.cs
+++ Assets/PlayerKeys.cs
@@ -4,15 +4,46 @@ using UnityEngine;
 
 public class PlayerKeys : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float runningSpeed = 2f;
+    public Rigidbody2D playerBody;
+
+    void Update ()
     {
-        
+        if(Input.GetKey("d"))
+        {
+            moveRight();
+        }
+        else if (Input.GetKey("a"))
+        {
+            moveLeft();
+        }
+        else if(Input.GetKey("w"))
+        {
+            moveUp();
+        }
+        else if (Input.GetKey("s"))
+        {
+            moveDown();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void moveLeft()
     {
-        
+        playerBody.AddForce(Vector2.left * (runningSpeed * Time.deltaTime), ForceMode2D.Force);
+    }
+
+    private void moveRight()
+    {
+        playerBody.AddForce(Vector2.right * (runningSpeed*Time.deltaTime), ForceMode2D.Force);
+    }
+
+    private void moveUp()
+    {
+        playerBody.AddForce(Vector2.up * (runningSpeed * Time.deltaTime), ForceMode2D.Force);
+    }
+
+    private void moveDown()
+    {
+        playerBody.AddForce(Vector2.down * (runningSpeed*Time.deltaTime), ForceMode2D.Force);
     }
 }
--- Assets/Scripts/Map.cs
+++ Assets/Scripts/Map.cs
@@ -7,39 +7,22 @@ public class Map : MonoBehaviour
 {
     public Tilemap tiles;
     public Tile switchTile;
+    private Sprite player;
     //public Sprite player;
     void Start(){
-        
+        //originTile = Instantiate(tiles.GetTile(new Vector3Int(0,0,0)));
+        //original = Instantiate(tiles);
     }
-    /*private Tile[,] tiles;
-    public int width = 20;
-    public int height = 10;
-    public double tileWidth = 1.0;
-    public double tileHeight = 1.0;
-    // Start is called before the first frame update
-    void Start()
-    {
-        tiles = new Tile[width, height];
-        for(int col = 0; col<tiles.GetLength(0); col++){
-            for(int row = 0; row<tiles.GetLength(1); row++){
-                double startX = 0.0-(width/2)*tileWidth+tileWidth/2;
-                double startY = 0.0+(height/2)*tileHeight-tileHeight/2;
-                double tileX = startX+col*tileWidth;
-                double tileY = startY-row*tileHeight;
-                tiles[col, row] = new Tile(tileX, tileY);
-            }
-        }
-        https://answers.unity.com/questions/1546818/how-can-i-change-a-tile-color-in-unity-by-using-c.html
-    }*/
-
     // Update is called once per frame
     void Update()
     {
         //Color colour = Color.red;//new Color(255.0f, 95.0f, 31.0f);
-        Vector3Int position = new Vector3Int(0,0,0);//player.transform.position;
+        
+        Vector3Int position = Vector3Int.FloorToInt(GameObject.Find("Player").transform.position);
         //tiles.SetTileFlags(position, TileFlags.None);
 
         tiles.SetTile(position, switchTile);
         //tiles.SetColor(position, colour);
     }
+
 }
