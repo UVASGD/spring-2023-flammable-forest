a398db1f005b2ebe0d150dbeae13f76c5f731cbe
ericli5 <lieric021@gmail.com>
2023-04-23 15:25:32 -0400

    Added trail

--- Assets/Scripts/Map.cs
+++ Assets/Scripts/Map.cs
@@ -7,22 +7,37 @@ public class Map : MonoBehaviour
 {
     public Tilemap tiles;
     public Tile switchTile;
+    public Tile lightTile;
+    //public Tile lightestTile;
     [SerializeField] private GameObject player;
     //public Sprite player;
     void Start(){
-        //originTile = Instantiate(tiles.GetTile(new Vector3Int(0,0,0)));
-        //original = Instantiate(tiles);
     }
     // Update is called once per frame
     void Update()
     {
         //Color colour = Color.red;//new Color(255.0f, 95.0f, 31.0f);
-        
-        Vector3Int position = Vector3Int.FloorToInt(player.transform.position);
-        //tiles.SetTileFlags(position, TileFlags.None);
+        if(Input.GetKey("space")){
+            Vector3Int position = Vector3Int.FloorToInt(player.transform.position);
+            position = Vector3Int.Scale(position, new Vector3Int(2, 2, 2));
 
-        tiles.SetTile(position, switchTile);
-        //tiles.SetColor(position, colour);
+            for(int y = -3; y <= 3; y++){
+                for(int x = -3; x <= 3; x++){
+                    Vector3Int nPos = position + new Vector3Int(x, y, 0);
+                    float distance = Vector3Int.Distance(position, nPos);
+                    if(distance <= 3){
+                        tiles.SetTile(nPos, lightTile);
+                    }
+                    /*if(distance <= 2){
+                        tiles.SetTile(nPos, lightTile);
+                    }*/
+                    if(distance <= 1){
+                        tiles.SetTile(nPos, switchTile);
+                    }
+                }
+            }
+            //tiles.SetTile(position, switchTile);
+        }
     }
 
 }
