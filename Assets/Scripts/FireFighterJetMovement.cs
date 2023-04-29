e7704941479e5d32e85ab19296a06410d8574fa2
JoeyGiordano <jgzgiordano@gmail.com>
2023-04-29 02:30:20 -0400

    Made sure everything is functioning and stuff

--- Assets/Scripts/FireFighterJetMovement.cs
+++ Assets/Scripts/FireFighterJetMovement.cs
@@ -6,7 +6,9 @@ public class FireFighterJetMovement : MonoBehaviour
 {
     public float rotateSpeed = 1, moveSpeed = 1, startLinDrag = 7, maxLinDrag = 9, treeDrag = 15;
     public float baseRotateSpeed = 160, highRotateSpeed = 250;
+    private double leftBound = -30.0, rightBound = 180.0, highBound = 50.0, lowBound = -70.0;
     Rigidbody2D rb;
+    private bool turned = false;
 
     [SerializeField] WaterSprayNozzle waterSprayNozzle;
     // Start is called before the first frame update
@@ -18,6 +20,8 @@ public class FireFighterJetMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Vector3 position = transform.position;
+
         //more linear drag while turning
         rb.drag = (maxLinDrag) - (maxLinDrag-startLinDrag) / (Mathf.Abs(rb.angularVelocity) + 1);
 
@@ -32,9 +36,33 @@ public class FireFighterJetMovement : MonoBehaviour
             rb.AddTorque(-1*rotateSpeed);
         }
 
-        rb.AddForce(moveSpeed * transform.up);
+        if (position.x < leftBound || position.x > rightBound || position.y < lowBound || position.y > highBound)
+        {
+            if (!turned)
+            {
+                turned = true;
 
-        
+                Debug.Log(transform.position);
+                Debug.Log(rb.rotation);
+                if (position.y < lowBound || position.y > highBound)
+                {
+                    rb.rotation = 180 - rb.rotation;
+                }
+                else
+                {
+                    rb.rotation = -rb.rotation;
+                }
+                rb.velocity = -1 * rb.velocity;
+
+            }
+        }
+        else
+        {
+            rb.AddForce(moveSpeed * transform.up);
+            turned = false;
+        }
+
+
         if (Input.GetKeyDown(KeyCode.Space))
         {
             waterSprayNozzle.OpenSprayNozzle();
