d83532f63968b8212bb5424f9e66892189ddf063
ericli5 <lieric021@gmail.com>
2023-04-28 22:49:49 -0400

    Added map basics

--- Assets/Scripts/Map.cs
+++ Assets/Scripts/Map.cs
@@ -8,18 +8,33 @@ public class Map : MonoBehaviour
     public Tilemap tiles;
     public Tile switchTile;
     public Tile lightTile;
-    //public Tile lightestTile;
+    public int xOffset;
+    public int yOffset; 
+    public string keyType;
     [SerializeField] private GameObject player;
-    //public Sprite player;
+    private Vector3Int prevPosition;
+    bool pressed = false;
     void Start(){
     }
     // Update is called once per frame
     void Update()
     {
-        //Color colour = Color.red;//new Color(255.0f, 95.0f, 31.0f);
-        if(Input.GetKey("space")){
-            Vector3Int position = Vector3Int.FloorToInt(player.transform.position);
-            position = Vector3Int.Scale(position, new Vector3Int(2, 2, 2));
+        Vector3Int position = Vector3Int.FloorToInt(player.transform.position);
+        position = Vector3Int.Scale(position, new Vector3Int(2, 2, 2));
+        position = position + new Vector3Int(-xOffset, -yOffset, 0);
+        if(Input.GetKey(keyType)){
+
+            for(int y = -3; y <= 3; y++){
+                for(int x = -3; x <= 3; x++){
+                    if(prevPosition != null){
+                        Vector3Int nPos = prevPosition + new Vector3Int(x, y, 0);
+                        float distance = Vector3Int.Distance(prevPosition, nPos);
+                        if(distance <= 1){
+                            tiles.SetTile(nPos, lightTile);
+                        }
+                    }
+                }
+            }
 
             for(int y = -3; y <= 3; y++){
                 for(int x = -3; x <= 3; x++){
@@ -28,15 +43,30 @@ public class Map : MonoBehaviour
                     if(distance <= 3){
                         tiles.SetTile(nPos, lightTile);
                     }
-                    /*if(distance <= 2){
-                        tiles.SetTile(nPos, lightTile);
-                    }*/
                     if(distance <= 1){
                         tiles.SetTile(nPos, switchTile);
                     }
                 }
             }
+
+            prevPosition = position;
             //tiles.SetTile(position, switchTile);
+            pressed = true;
+        }
+        else if(pressed == true){
+            pressed = true;
+
+            for(int y = -3; y <= 3; y++){
+                for(int x = -3; x <= 3; x++){
+                    if(prevPosition != null){
+                        Vector3Int nPos = prevPosition + new Vector3Int(x, y, 0);
+                        float distance = Vector3Int.Distance(prevPosition, nPos);
+                        if(distance <= 1){
+                            tiles.SetTile(nPos, lightTile);
+                        }
+                    }
+                }
+            }
         }
     }
 
